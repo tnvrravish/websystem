52203dd9331df0957561574add43a78bd1c3db4c
tnvrravish <vt7@njit.edu>
2022-02-07 15:32:05 -0500

    Updated to vishnu

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Vishnu  Thota"
         return render_template('index.html', name=name)
