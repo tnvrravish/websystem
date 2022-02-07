6991899d072c323418ba4b32cb66696842f8fa8a
tnvrravish <vt7@njit.edu>
2022-02-07 15:39:10 -0500

    Modified name to Vishnu Sai Thota

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Vishnu  Thota"
+        name = "Vishnu Sai Thota"
         return render_template('index.html', name=name)
